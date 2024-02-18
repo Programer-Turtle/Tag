298e8022cad8631e7cc5b73a11974d223ea21ca2
RAAAAAAAAAAAAAAAAATZ <karsonulerick@gmail.com>
2024-02-18 12:23:59 -0500

    NewUpdate

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var context = canvas.getContext('2d');
 
 var counter = 0;
 var lost = false;
+var WallAmount = 30
 
 function SetRandomIcon()
 {
@@ -34,6 +35,12 @@ var Enemy = {
     y: 192
 };
 
+var runner = 
+{
+    x: 192,
+    y: 192
+}
+
 var walls = []
 
 function abosluteValue(number)
@@ -61,6 +68,7 @@ function DrawWalls()
 
 function GenerateWallCords(amount)
 {
+    walls = []
     for (var index = 0; index < amount;)
     {
         var generatedcord = [getRandomInt(0, 25) * grid ,getRandomInt(0, 25) * grid]
@@ -94,6 +102,11 @@ function MoveEnemy()
     {
         return
     }
+    else
+    {
+        points = points+1
+        pointdisp.innerText = points;
+    }
 
     //move y
     if (apple.y === Enemy.y || abosluteValue(apple.y - Enemy.y) === grid && abosluteValue(apple.x - Enemy.x) > 1*grid)
@@ -124,12 +137,129 @@ function MoveEnemy()
     }
 }
 
+function MoveRunner()
+{
+    //Xposistion
+    var XPosistions = [runner.x, runner.x + grid, runner.x - grid]
+    var Xdifferences = []
+    var targetx = 0
+
+    //Checks if died
+    if((runner.y === apple.y) && (runner.x === apple.x))
+    {
+        points = points + 100
+        pointdisp.innerText = points;
+        runner.y  = getRandomInt(0, 25) * grid
+        runner.x  = getRandomInt(0, 25) * grid
+    }
+
+    //Distance Required
+    YDisntance = abosluteValue(apple.x - runner.x)
+    XDisntance = abosluteValue(apple.y - runner.y)
+    if (XDisntance >= (4 * grid) && YDisntance >= (4 * grid) || XDisntance <= (1 * grid) && YDisntance <= (1 * grid))
+    {
+        return
+    }
+
+    var IndexToRemoveX = []
+    //Removes Invalid Moves
+    for (var is = 0; is < XPosistions.length; is++)
+    {
+        if((XPosistions[is] <= -16 || XPosistions[is] >= 400) || (!AllowedToBeAtSpot(XPosistions[is], runner.y)))
+        {
+            IndexToRemoveX.push(is)
+        }
+    }
+
+    for (var i = 1; i <= IndexToRemoveX.length; i++)
+    {
+        XPosistions.splice(IndexToRemoveX[IndexToRemoveX.length - i],1)
+    }
+
+    //Get Distances
+    for (var index = 0; index < XPosistions.length; index++)
+    {
+        Xdifferences.push(abosluteValue(apple.x - XPosistions[index]))
+    }
+
+    //Gets Largest Difference
+    var largest = Xdifferences[0];
+
+    for (var i = 0; i < Xdifferences.length; i++) 
+    {
+        if (Xdifferences[i] > largest ) 
+        {
+            largest = Xdifferences[i];
+        }
+    }
+    targetx = XPosistions[Xdifferences.indexOf(largest)]
+
+    console.log(XPosistions)
+    console.log(targetx)
+    //Chnages Posistion
+    runner.x = targetx
+
+    //Yposistion
+    var YPosistions = [runner.y, runner.y + grid, runner.y - grid]
+    var Ydifferences = []
+    var targetY = 0
+
+    //Removes Invalid Moves
+    var IndexToRemoveY = []
+    for (var is = 0; is < YPosistions.length; is++)
+    {
+        if(YPosistions[is] <= -16 || YPosistions[is] >= 400 || !AllowedToBeAtSpot(runner.x, YPosistions[is]))
+        {
+            IndexToRemoveY.push(is)
+        }
+    }
+
+    for (var i = 1; i <= IndexToRemoveY.length; i++)
+    {
+        YPosistions.splice(IndexToRemoveY[IndexToRemoveY.length - i],1)
+    }
+
+    //Get Distances
+    for (var index = 0; index < YPosistions.length; index++)
+    {
+        Ydifferences.push(abosluteValue(apple.y - YPosistions[index]))
+    }
+
+    //Gets Largest Difference
+    var largest = Ydifferences[0];
+
+    for (var i = 0; i < Ydifferences.length; i++) 
+    {
+        if (Ydifferences[i] > largest ) 
+        {
+            largest = Ydifferences[i];
+        }
+    }
+    targetY = YPosistions[Ydifferences.indexOf(largest)]
+
+    //Chnages Posistion
+    runner.y = targetY
+}
+
+function Restart()
+{
+    apple.x = 0
+    apple.y = 0
+    Enemy.x = 16
+    Enemy.y = 192
+    points = 0
+    GenerateWallCords(WallAmount)
+    pointdisp.innerText = points;
+    lost = false
+}
+
 function CheckIfLost()
 {
     if (Enemy.x == apple.x && Enemy.y == apple.y)
     {
         lost = true
         context.clearRect(0, 0, canvas.width, canvas.height);
+        Restart()
     }
 }
 
@@ -144,15 +274,24 @@ function Draw()
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     //Draws
+
+    //Walls
     DrawWalls()
-    context.fillStyle = 'lime';
+
+    //Enemy
     MoveEnemy()
+    context.fillStyle = 'lime';
     context.fillRect(Enemy.x, Enemy.y, grid, grid);
+
+    //Runner
+    MoveRunner()
+    context.fillStyle = 'orange';
+    context.fillRect(runner.x, runner.y, grid, grid);
+
+    //Apple
     context.fillStyle = 'red';
     context.fillRect(apple.x, apple.y, grid, grid);
     CheckIfLost()
-    points = points+1
-    pointdisp.innerText = points;
 }
 
 document.addEventListener('keydown', function(e) {
@@ -204,4 +343,4 @@ document.addEventListener('keydown', function(e) {
 }, 150);
 
 SetRandomIcon()
-GenerateWallCords(10)
+GenerateWallCords(WallAmount)
